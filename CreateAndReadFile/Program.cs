623bf0518890c0116fd01ed4ae8449580bbf3c96
Juliaashr <81264191+Juliaashr@users.noreply.github.com>
2021-03-31 19:39:55 +0300

    Add files via upload

---
+++ CreateAndReadFile/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO.Compression;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateAndReadFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = new FileStream(@".\text.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            using (var writer = new StreamWriter(path, Encoding.UTF8))
+            {        
+                writer.WriteLine("Привет!");
+
+                writer.WriteLine("Rезиновый подлокотник эскалатора в метро двигается с другой скоростью для того, чтобы пассажир не уснул на эскаlаторе.");
+
+                Console.WriteLine("файл создан, текст занесен в него");
+            }   
+
+            using (StreamReader reader = File.OpenText(@".\text.txt"))
+            {
+                Console.WriteLine("попытка прочесть файл text.txt ...");
+
+                Console.WriteLine(reader.ReadToEnd());
+            }
+            Console.ReadLine();
+        }
+    }
+}
